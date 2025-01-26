446d691919c780fff853eee4588253e72ecd2508
tomdeocales <131328239+tomdeocales@users.noreply.github.com>
2025-01-26 19:08:03 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAD-wZBdqqpMrXng0BKFSIiJw9jzL_xzjA",
-  authDomain: "lake-d30ad.firebaseapp.com",
-  databaseURL: "https://lake-d30ad-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "lake-d30ad",
-  storageBucket: "lake-d30ad.appspot.com",
-  messagingSenderId: "633397665516",
-  appId: "1:633397665516:web:9545c24d528da466544fc9"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
